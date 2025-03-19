ef3170c108f1462a168d2ea9bdff027dab73d3ff
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2025-03-20 07:36:11 +0900

    ✨ commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,41 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes, useParams } from "react-router";
 
 import { RootLayout } from "./components/layouts";
 import { RootPage } from "./components/pages";
 import { ThemeProvider } from "./components/providers";
 
+const ManualPage: React.FC = () => {
+  const { projectName, "*": splat } = useParams();
+
+  return <>{`${projectName} ${splat}`}</>;
+};
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="theme">
-      <BrowserRouter>
+      <BrowserRouter basename="manu">
         <Routes>
-          <Route path="manu" element={<RootLayout />}>
+          {/* / */}
+          <Route path="/" element={<RootLayout />}>
             <Route index element={<RootPage />} />
-            <Route path="*" element={<>root *</>} />
+
+            {/* /:projectName */}
+            <Route path=":projectName">
+              <Route index element={<ManualPage />} />
+
+              {/* /:projectName/- */}
+              <Route path="-">
+                <Route index element={<Navigate to="/" />} />
+
+                {/* /:projectName/-/* */}
+                <Route path="*" element={<ManualPage />} />
+              </Route>
+
+              {/* /:projectName/* */}
+              <Route path="*" element={<Navigate to="/" />} />
+            </Route>
           </Route>
 
           <Route path="*" element={<>not found</>} />
