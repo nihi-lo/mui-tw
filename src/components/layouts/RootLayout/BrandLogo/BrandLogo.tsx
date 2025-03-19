b3af017afc3239ff561579db5983be888c16235e
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2025-03-20 00:19:47 +0900

    ✨ commit

--- src/components/layouts/RootLayout/BrandLogo/BrandLogo.tsx
+++ src/components/layouts/RootLayout/BrandLogo/BrandLogo.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router";
 
-export const BrandLogo: React.FC = () => {
+const BrandLogo: React.FC = () => {
   return (
     <Link to="/">
       <span className="text-xl font-bold">MUI + TW</span>
     </Link>
   );
 };
+
+export { BrandLogo };
