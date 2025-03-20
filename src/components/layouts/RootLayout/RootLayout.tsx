46043f122fadde54a8f25939315cf9d657573305
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2025-03-21 01:29:44 +0900

    ✨ commit

--- src/components/layouts/RootLayout/RootLayout.tsx
+++ src/components/layouts/RootLayout/RootLayout.tsx
@@ -6,7 +6,7 @@ import { ModeToggle } from "./ModeToggle";
 const RootLayout: React.FC = () => {
   return (
     <>
-      <header className="bg-background sticky top-0 z-10 flex h-14 items-center justify-between px-4">
+      <header className="bg-background sticky top-0 z-10 flex h-14 items-center justify-between px-4 shadow-md">
         <BrandLogo />
         <ModeToggle />
       </header>
